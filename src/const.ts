a5eaaa59729cf1001446f3b64587c3b9b879e593
istianmws <istianmuhammad@gmail.com>
2024-06-12 02:09:33 +0700

    problem section

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-import FactSection from "./components/icons/FactSection.astro";
-import FactSection1 from "./components/icons/FactSection1.astro";
 
 export const siteConfig = {
     SITE_TITLE: 'Qissa ID',
@@ -10,29 +8,7 @@ export const siteConfig = {
     headlineH1: 'Jasa Kelola Akun Tiktok',
     headlineH2: 'Konten FYP, Jualan Makin Pede ',
     headlineP: 'Solusi manajemen, naikkan view dan follower akun tiktok biar bisnis makin eksis',
+    problemP: 'Hal inilah yang mungkin membuat para bisnis owner seperti anda, melakukan hal-hal berikut ini:',
+    problemH1: 'Masalah yang sering dihadapi',
+    problemH2: 'Yang itu semua mengakibatkan brand bisnis anda kurang dikenal luas, sulit mendapatkan prospek atau calon pelanggan, hingga peningkatan omset kurang signifikan.'
 };
-
-export const factSection = [
-    {
-        title: "Pengen sosmednya rame",
-        description: "Punya akun sosmed / tiktok yang berfollower real person dan aktif berinteraksi",
-        icon: FactSection
-    },
-
-    {
-        title: "Viral dan terkenal",
-        description: "Jangkauan konten yang luas, brand bisnis anda dikenal sehingga meningkatkan penjualan",
-        icon: FactSection1
-    },
-    {
-        title: "Efisiensi biaya",
-        description: "Hemat gaji karyawan untuk kelola sosmed, anda bisa pakai budgetnya untuk keperluan lain",
-        icon: FactSection
-    },
-    {
-        title: "Branding identity",
-        description: "Brand lebih depat dikenal lewat sosial media termasuk tiktok, sehingga sosmed tidak bisa anda tinggalkan",
-        icon: FactSection1
-    }
-
-];
