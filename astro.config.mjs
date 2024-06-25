7d085f693ccc047a79c6b419667257fb62a9b88a
istianmws <istianmuhammad@gmail.com>
2024-06-25 11:20:55 +0700

    sync files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-import node from "@astrojs/node";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,4 @@
 
-
 module.exports = {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
